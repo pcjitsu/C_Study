01301c923505d645e0a96b2c8b12c79c099243a8
tony <45546763+pcjitsu@users.noreply.github.com>
2026-01-08 21:18:15 -0600

    C Book

--- main.c
+++ main.c
@@ -1,9 +1,12 @@
 #include <stdio.h>
+#include "bss.h"
 
-int main(int argc, char const *argv[])
+int main()
 {
-    printf("Hellow World");
+    int myFavoriteNumber = strong;
+    int myFavoriteNum;
+    printf("Please enter your fav number");
+    scanf("%d", &myFavoriteNum);
+    printf("You entered : %d and also %d \n", myFavoriteNumber, myFavoriteNum);
     return 0;
 }
-
-// Test
